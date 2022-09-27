90c8f4ebd78b77cfee94d9397d49e7b97f04cc41
LilyLJWang <1427688059@qq.com>
2022-09-27 23:57:22 +0800

    wlj

--- homework1.py
+++ homework1.py
@@ -1,3 +1,21 @@
-w=1
-print(w)
-w=2
+import time
+import multiprocessing
+
+def eat(num,name):
+    for i in range(num):
+        print(name+"吃一口...")
+        time.sleep(1)
+
+def drink(num,name):
+    for i in range(num):
+        print(name+"喝一口...")
+        time.sleep(1)
+
+if __name__ == '__main__':
+    eat_process=multiprocessing.Process(target=eat,args=(3,"giao"))
+    drink_process=multiprocessing.Process(target=drink,kwargs={"num":4,"name":"giao"})
+    eat_process.start()
+    drink_process.start()
+
+
+
